dd0371baf5d3c0036a7644173393a3582a32f413
Ivan Demchenko <idemchenko.js@gmail.com>
2024-01-24 10:17:10 +0100

    feat: handle form data (without csrf)

--- bin/route_handlers.ml
+++ bin/route_handlers.ml
@@ -4,12 +4,17 @@ let index_page = fun _ ->
   Template.index_page
   |> Dream.html
 
-let new_todo = fun _ ->
-  let todo = {
-    id = "1-abc";
-    completed = true;
-    name = "a new todo";
-  }
-  in
-  Template.todo_item todo
-  |> Dream.html 
+let new_todo = (fun request ->
+  match%lwt Dream.form ~csrf:false request with
+  | `Ok ["todo.name", todoName] ->
+    let todo = {
+      id = "1-abc";
+      completed = true;
+      name = todoName;
+    }
+    in
+    Template.todo_item todo
+    |> Dream.html
+  | _ ->
+    Dream.empty `Bad_Request
+  )
